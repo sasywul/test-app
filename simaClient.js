08dbba00a5610e8a228ff501e65e231a7de131d2
Rafi Dzaky Maulana A  P <adeedo12345@gmail.com>
2025-12-27 08:12:14 +0700

    client

--- simaClient.js
+++ simaClient.js
@@ -7,7 +7,6 @@ const { wrapper } = require("axios-cookiejar-support");
 /* =========================
    CLIENT SETUP
 ========================= */
-
 function createClient() {
   const jar = new CookieJar();
 
@@ -15,13 +14,33 @@ function createClient() {
     axios.create({
       jar,
       withCredentials: true,
+      timeout: 30000,
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+
+        "Accept":
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+
+        "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8",
+
+        "Accept-Encoding": "gzip, deflate, br",
+
+        "Connection": "keep-alive",
+
+        "Upgrade-Insecure-Requests": "1",
+
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-User": "?1",
+
+        "Referer": "https://sima.usm.ac.id/"
       }
     })
   );
 }
+
 
 /* =========================
    LOGIN
